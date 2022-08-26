5082f1c8403ed183bfd307595f87fb53bd60e40e
Sandhiya2 <89214877+Sandhiya2@users.noreply.github.com>
2022-08-26 20:43:07 +0530

    success

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysqli_connect('localhost','root','','shopping');
+// Check connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
